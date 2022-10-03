d9dd7adb836d9ec9a72ab00d52f636fd4ee25f0a
farhan sharif <farhansharif1410@gmail.com>
2022-10-03 21:51:59 +0530

    line chart added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Navbar from './component/Navbar/Navbar';
 import Pricing from './component/Pricing/Pricing';
+import Reachart from './component/Reachart/Reachart';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <h1 className='text-4xl font-bold underline '>Navbar in Tailwind Css</h1>
       <h1 className='text-3xl '>This is another tag</h1>
       <Pricing />
+      <Reachart />
     </div>
   );
 }
